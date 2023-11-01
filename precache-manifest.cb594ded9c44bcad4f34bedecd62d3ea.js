c17a223ed671d42711f8de3d2679c9a4ebbfa65a
Miguel Sosa <msosadesign@gmail.com>
2023-11-01 15:14:09 +0100

    Updates

---
+++ precache-manifest.cb594ded9c44bcad4f34bedecd62d3ea.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f123799a92bf659a891046d2ecfe0a41",
+    "url": "/react-deploy-test/index.html"
+  },
+  {
+    "revision": "e69449dc74f5bc219220",
+    "url": "/react-deploy-test/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "590987dd26149de296b8",
+    "url": "/react-deploy-test/static/js/2.2ca6b2bb.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/react-deploy-test/static/js/2.2ca6b2bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a072033ecbb367b3e42",
+    "url": "/react-deploy-test/static/js/3.e5658f37.chunk.js"
+  },
+  {
+    "revision": "e69449dc74f5bc219220",
+    "url": "/react-deploy-test/static/js/main.eab95855.chunk.js"
+  },
+  {
+    "revision": "2a28c577d9857be715ff",
+    "url": "/react-deploy-test/static/js/runtime-main.d5b9068e.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/react-deploy-test/static/media/logo.06e73328.svg"
+  }
+]);
